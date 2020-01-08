449d273bdd97bd3c4be7255b3930e987b8c4c231
Grace0 <gracekwak25@gmail.com>
2020-01-07 17:52:11 -0800

    css for homepage

--- src/App.js
+++ src/App.js
@@ -11,16 +11,18 @@ class App extends Component {
     <div className="App">
       <header className="App-header">
       <h1 className="App-title">Erin Lee</h1>
-<p>
-  <img src={ require('./drawing-transparent.png') } />
-</p>
-      <nav>
-   <ul>
-     <li><Link to='/quotes'>Quotes</Link></li>
-     <li><Link to='/rundown'>Rundown</Link></li>
-     <li><Link to='/plans'>Plans</Link></li>
-   </ul>
- </nav>
+        <p>
+          <img src={ require('./drawing-transparent.png') } style={{width: 200}} />
+        </p>
+        <p>
+      <nav className="navbar">
+        <ul>
+          <li><Link to='/quotes'>Quotes</Link></li>
+          <li><Link to='/rundown'>Rundown</Link></li>
+          <li><Link to='/plans'>Plans</Link></li>
+        </ul>
+      </nav>
+      </p>
       </header>
       <Switch>
         <Route path='/quotes' component={Quotes}/>
