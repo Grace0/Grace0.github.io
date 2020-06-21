839fe9c464736d46996deaa89a85ba93d957e801
Grace0 <gracekwak25@gmail.com>
2020-06-21 10:34:12 -0700

    homepage content redone

--- src/App.js
+++ src/App.js
@@ -16,13 +16,15 @@ class App extends Component {
 
       <p>
 
-
       <main class="grid-container home">
        <div class='grid-100 mobile-grid-100 tablet-grid-100'>
-       <h1> <img src={ require('./img/drawing-transparent.png') } style={{width: 200}} /> Hello! I'm Grace Kwak. Here's what I am
-          <NavLink to='/quotes' className="quotes"> making</NavLink>,
-          <NavLink to='/rundown' className="rundown"> reading</NavLink>, and
-          <NavLink to='/plans' className="plans"> working on</NavLink>. </h1>
+       <h1> Hello there! I'm Grace, a first-year Electrical Engineering student at UCLA. I'm eager to demystify the theories behind hardware in order to build products with a high social impact.
+
+       <p> Here is my
+          <NavLink to='/quotes' className="quotes"> resume </NavLink> and
+          <NavLink to='/rundown' className="rundown"> portfolio</NavLink> thus far, as well as a snapshot of my current
+          <NavLink to='/plans' className="plans"> aspirations</NavLink>. </p></h1>
+
        </div>
       </main>
 
