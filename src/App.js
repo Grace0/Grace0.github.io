972fc807cfa590cec5bbb975388c7cfaf5cf435d
Grace Kwak <gracekwak@Graces-MacBook-Pro.local>
2020-08-11 11:47:49 -0700

    add notion subpages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React, {Component} from 'react';
 import {Switch, Route, NavLink} from 'react-router-dom';
 import './App.css';
 import Homepage from './components/Homepage';
-import RouteManager from './components/RouteManager';
 
 function App() {
   return (
     <div>
-    App
       <Switch>
         <Route path='/' exact component={Homepage}/>
-        <Route path='/*' component={RouteManager}/>
       </Switch>
     </div>
   );
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,21 +7,15 @@ function Homepage() {
       <header className="App-header">
         <main className="grid-container home">
           <h1>
-            {" "}
-            Hello there! I'm Grace, a first-year Electrical Engineering student
-            at UCLA. I'm tinkering with hardware and advancing social change.
-            Here is my{" "}
-            <NavLink to="/resume" className="resume">
-              resume
-            </NavLink>{" "}
-            and{" "}
-            <NavLink to="/portfolio" className="portfolio">
-              portfolio
-            </NavLink>
-            , as well as a list of my big-picture{" "}
-            <NavLink to="/aspirations" className="aspirations">
-              aspirations
-            </NavLink>
+          <p>Grace Kwak </p>
+            I'm a first-year Electrical Engineering student
+            at UCLA. I love tinkering with hardware and advocating for social change.
+            To see what I've been up to, take a look at my {" "}
+            <a href="https://www.notion.so/Resume-41cc0b7cb9dd4c5799f9d5d6f04461d7" className="resume">resume</a>
+            {" "}and{" "}
+            <a href="https://www.notion.so/Portfolio-137e33c2e41741c5ae446d560d314eab" className="portfolio">portfolio</a>
+             ; to see where I'm going next, explore my {" "}
+            <a href="https://www.notion.so/Aspirations-c1a6526899dd40a18bf7b6a9a992bbe3" className="aspirations">aspirations</a>
             .
           </h1>
         </main>
